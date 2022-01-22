e026dbb0425f5522fe64bd5d5c4d5b33aa1238e7
weichen <w3ichen@gmail.com>
2022-01-22 09:20:11 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,6 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'MecSim', // Usually your GitHub org/user name.
   projectName: 'MecSimCalc-docs', // Usually your repo name.
-  deploymentBranch: "main",
   trailingSlash: false,
 
   presets: [
