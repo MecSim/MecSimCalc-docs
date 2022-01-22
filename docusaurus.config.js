7a5dcf64261c8858fbb62f3f75ff02803cb37452
weichen <w3ichen@gmail.com>
2022-01-21 22:56:51 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'MecSimCalc Docs',
   tagline: 'The simplest way to build and share computational tools on the web. Create and share your python apps in minutes using our intuitive and simple platform.',
-  url: 'docs.mecsimcalc.com',
-  baseUrl: '/MecSimCalc-docs/',
+  url: 'https://docs.mecsimcalc.com',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
