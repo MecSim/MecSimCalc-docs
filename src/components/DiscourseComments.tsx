47c017b32c6a6d7e9edd8a43a07fd2c0af1c942b
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2022-07-27 21:09:58 -0600

    Try <%= current_page.url %>

--- src/components/DiscourseComments.tsx
+++ src/components/DiscourseComments.tsx
@@ -1,12 +1,15 @@
 import Head from "@docusaurus/Head";
 import React from "react";
 
+/**
+ * https://meta.discourse.org/t/embedding-discourse-comments-via-javascript/31963
+ */
 export default function DiscourseComments() {
   if (typeof window == "undefined") return null;
 
   (window as any).DiscourseEmbed = {
     discourseUrl: "https://community.mecsimcalc.com/",
-    discourseEmbedUrl: window.location.href,
+    discourseEmbedUrl: `${window.location.origin}<%= current_page.url %>`,
   };
   return (
     <div id="discourse-comments" style={{ marginTop: "50px" }}>
