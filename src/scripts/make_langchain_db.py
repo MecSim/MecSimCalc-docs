fb68f144a1df0ef28b47e6632fc08cf67f914b86
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2023-10-24 11:24:28 -0600

    make_langchain_db.py
    create langchain_db.pkl
    
    Langchain db changes
    
    removed the .. from the script
    
    fix path issues
    
    make langchain_db include blogs as well
    
    try w/o branch protections
    
    forced push
    
    uses checkout
    
    fixing yml
    
    langchain db

---
+++ src/scripts/make_langchain_db.py
@@ -0,0 +1,45 @@
+"""
+Make langchain vector DB for AI to learn from
+https://python.langchain.com/docs/use_cases/question_answering/vector_db_text_generation
+"""
+import os
+import pickle
+import pathlib
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+
+
+def get_github_docs():
+    markdown_files = []
+    for dir in ["../../docs", "../../blog"]:
+        repo_path = pathlib.Path(os.path.abspath(dir))
+        markdown_files.extend(
+            list(repo_path.glob("**/*.md")) + list(repo_path.glob("**/*.mdx"))
+        )
+
+    for markdown_file in markdown_files:
+        print("->", markdown_file)
+        with open(markdown_file, "r") as f:
+            yield Document(
+                page_content=f.read(),
+                metadata={"source": "/".join(str(markdown_file).split("/")[-2:])},
+            )
+
+
+if __name__ == "__main__":
+    sources = get_github_docs()
+
+    source_chunks = []
+    splitter = CharacterTextSplitter(separator=" ", chunk_size=1024, chunk_overlap=0)
+    for source in sources:
+        for chunk in splitter.split_text(source.page_content):
+            source_chunks.append(Document(page_content=chunk, metadata=source.metadata))
+
+    with open(os.path.abspath("../../static/assets/langchain_db.pkl"), "wb") as f:
+        pickle.dump(source_chunks, f, protocol=5)
+        print("<- Saved langchain_db.pkl")
+
+    with open(os.path.abspath("../../static/assets/langchain_db.pkl"), "rb") as f:
+        source_chunks = pickle.load(f)
+        print("<- Tested langchain_db.pkl")
+        print(f"<- {len(source_chunks)} documents")
