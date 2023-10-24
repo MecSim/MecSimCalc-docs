b9e0c836bd7390ad2316a2a5720070930e347bf1
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2023-10-24 11:27:13 -0600

    Langchain db changes
    removed the .. from the script
    
    fix path issues
    
    make langchain_db include blogs as well
    
    try w/o branch protections
    
    forced push
    
    uses checkout
    
    fixing yml

--- src/scripts/make_langchain_db.py
+++ src/scripts/make_langchain_db.py
@@ -1,5 +1,6 @@
 """
 Make langchain vector DB for AI to learn from
+Run from root folder: `python src/scripts/make_langchain_db.py`
 https://python.langchain.com/docs/use_cases/question_answering/vector_db_text_generation
 """
 import os
@@ -10,14 +11,19 @@ from langchain.text_splitter import CharacterTextSplitter
 
 
 def get_github_docs():
-    repo_path = pathlib.Path(os.path.abspath("../../docs"))
-    markdown_files = list(repo_path.glob("*/*.md")) + list(repo_path.glob("*/*.mdx"))
+    markdown_files = []
+    for dir in ["docs", "blog"]:
+        repo_path = pathlib.Path(os.path.abspath(dir))
+        markdown_files.extend(
+            list(repo_path.glob("**/*.md")) + list(repo_path.glob("**/*.mdx"))
+        )
+
     for markdown_file in markdown_files:
         print("->", markdown_file)
         with open(markdown_file, "r") as f:
             yield Document(
                 page_content=f.read(),
-                metadata={"source": str(markdown_file.relative_to(repo_path))},
+                metadata={"source": "/".join(str(markdown_file).split("/")[-2:])},
             )
 
 
@@ -30,11 +36,11 @@ if __name__ == "__main__":
         for chunk in splitter.split_text(source.page_content):
             source_chunks.append(Document(page_content=chunk, metadata=source.metadata))
 
-    with open("langchain_db.pkl", "wb") as f:
+    with open(os.path.abspath("static/assets/langchain_db.pkl"), "wb") as f:
         pickle.dump(source_chunks, f, protocol=5)
         print("<- Saved langchain_db.pkl")
 
-    with open("langchain_db.pkl", "rb") as f:
+    with open(os.path.abspath("static/assets/langchain_db.pkl"), "rb") as f:
         source_chunks = pickle.load(f)
         print("<- Tested langchain_db.pkl")
         print(f"<- {len(source_chunks)} documents")
